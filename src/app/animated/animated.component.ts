18d2b1aaa77fba08644c846909fe5f582773fe62
jajet.loh <jajetloh@gmail.com>
2022-06-15 00:53:04 +0800

    transitions working properly

--- src/app/animated/animated.component.ts
+++ src/app/animated/animated.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core'
-import { animate, state, style, transition, trigger } from "@angular/animations"
+import { animate, style, transition, trigger } from "@angular/animations"
+
+// Based on: https://www.usefuldev.com/post/Angular:%20using%20animations%20with%20NgIf
 
 export type AnimationState = 'visible' | 'hidden'
+export type AnimationDirection = 'forward' | 'backward'
 
 @Component({
     selector: 'app-animated',
@@ -9,17 +12,32 @@ export type AnimationState = 'visible' | 'hidden'
     styleUrls: ['./animated.component.css'],
     animations: [
         trigger('state', [
-            state('visible',
-                style({ opacity: 1 })
-            ),
-            state('hidden',
-                style({ opacity: 0 })
-            ),
-            transition('* => visible', [
-                animate('500ms ease-out')
+            // state('visible_forward, visible_backward',
+            //     style({ opacity: 1 })
+            // ),
+            // state('hidden_forward, hidden_backward',
+            //     style({ opacity: 0 })
+            // ),
+            // transition('void => visible', [
+            //     style({ opacity: 0 }),
+            //     animate('400ms ease-out', style({ opacity: 1 })),
+            // ]),
+            // transition('visible => hidden', [
+            //     animate('400ms ease-out', style({ opacity: 0 })),
+            // ]),
+            transition('void => visible_forward', [
+                style({ opacity: 0, transform: 'translateX(100px)', position: 'absolute', top: 0, "transform-origin": "center center" }),
+                animate('400ms ease-out', style({ opacity: 1, transform: 'translateX(-123px)', "transform-origin": "center center" })),
             ]),
-            transition('visible => hidden', [
-                animate('500ms ease-out')
+            transition('void => visible_backward', [
+                style({ opacity: 0, transform: 'translateX(-100px)', position: 'absolute', top: 0, "transform-origin": "center center" }),
+                animate('400ms ease-out', style({ opacity: 1, transform: 'translateX(123px)', "transform-origin": "center center" })),
+            ]),
+            transition('visible_forward => hidden_forward, visible_backward => hidden_forward', [
+                animate('400ms ease-out', style({ opacity: 0, transform: 'translateX(-223px)' })),
+            ]),
+            transition('visible_forward => hidden_backward, visible_backward => hidden_backward', [
+                animate('400ms ease-out', style({ opacity: 0, transform: 'translateX(223px)' })),
             ]),
         ])
     ],
@@ -28,7 +46,9 @@ export type AnimationState = 'visible' | 'hidden'
 export class AnimatedComponent {
 
     state: AnimationState = 'visible'
-    private _show: boolean = true
+    // @Input() direction: AnimationDirection = 'forward'
+    private _show: boolean = false
+    @Input() direction: AnimationDirection = 'forward'
     get show() {
         return this._show
     }
@@ -41,8 +61,16 @@ export class AnimatedComponent {
         }
     }
 
+    // get direction() {
+    //     return this._direction
+    // }
+    //
+    // @Input() set direction(value: AnimationDirection) {
+    //
+    // }
+
     animationDone(event: any) {
-        if (event.fromState === 'visible' && event.toState === 'hidden') {
+        if (event.fromState.split('_')[0] === 'visible' && event.toState.split('_')[0] === 'hidden') {
             this._show = false
         }
     }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,42 @@
 import { Component } from '@angular/core'
 import { animate, state, style, transition, trigger } from "@angular/animations"
+import { AnimationDirection } from "./animated/animated.component"
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     animations: [
-        trigger('toggleText', [
-            transition('void => forward', [
-                style({ opacity: 0, transform: 'translateX(100%)' }),
-                animate('0.5s', style({ opacity: 1, transform: 'translateX(0)' }))
-            ]),
-            transition('forward => void', [
-                animate('0.5s', style({ opacity: 0, transform: 'translateX(-100%)' }))
-            ]),
-            transition('void => backward', [
-                style({ opacity: 0, transform: 'translateX(-100%)' }),
-                animate('0.5s', style({ opacity: 1, transform: 'translateX(0)' }))
-            ]),
-            transition('backward => void', [
-                animate('0.5s', style({ opacity: 0, transform: 'translateX(100%)' }))
-            ])
-        ])
+        // trigger('toggleText', [
+        //     transition('void => forward', [
+        //         style({ opacity: 0, transform: 'translateX(100%)' }),
+        //         animate('0.5s', style({ opacity: 1, transform: 'translateX(0)' }))
+        //     ]),
+        //     transition('forward => void', [
+        //         animate('0.5s', style({ opacity: 0, transform: 'translateX(-100%)' }))
+        //     ]),
+        //     transition('void => backward', [
+        //         style({ opacity: 0, transform: 'translateX(-100%)' }),
+        //         animate('0.5s', style({ opacity: 1, transform: 'translateX(0)' }))
+        //     ]),
+        //     transition('backward => void', [
+        //         animate('0.5s', style({ opacity: 0, transform: 'translateX(100%)' }))
+        //     ])
+        // ])
     ],
 })
 export class AppComponent {
 
     currentTextIndex = 0
-    goingForward: 'forward' | 'backward' = 'forward'
+    goingForward: AnimationDirection = 'forward'
     textList = [
         'First thing',
         'Second thing',
         'Third thing',
+        'Fourth thing',
+        'Fifth thing',
+        'Sixth thing',
+        'Seventh thing',
     ]
 
     constructor() {
